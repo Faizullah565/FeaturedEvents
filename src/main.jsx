67b40656fd5de3e5bd4b4da1616aa55735dbe66b
faizullah <faizullah@gmail.com>
2026-01-02 22:28:16 +0500

    Updated UI and event pages

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 {/* <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script> */}
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-
 import '@fortawesome/fontawesome-free/css/all.min.css';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
